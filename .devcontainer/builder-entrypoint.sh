e17346a822e82c3f5fd00e283be36800b98e961d
Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>
2026-01-12 10:47:59 +0100

    Ensure out/sim folder is created when running builder.
    Signed-off-by: Timothy Rule (VM/EMT3) <Timothy.Rule@de.bosch.com>

--- .devcontainer/builder-entrypoint.sh
+++ .devcontainer/builder-entrypoint.sh
@@ -10,7 +10,17 @@ SIM_NAME="${DSE_FILE%.*}"
 
 echo "Building simulation: $SIM_NAME"
 
+# Builder part.
 dse-parse2ast "$DSE_FILE" "${SIM_NAME}.json"
 dse-ast convert -input "${SIM_NAME}.json" -output "${SIM_NAME}.yaml"
 dse-ast resolve -input "${SIM_NAME}.yaml"
 dse-ast generate -input "${SIM_NAME}.yaml" -output .
+
+# Ensure sim folder is created (required by Task).
+mkdir -p out/sim
+
+# Run Task.
+TASK_X_REMOTE_TASKFILES=1
+# TODO: task -y -v
+# TODO: DinD volume mapping etc.
+# TODO: Control by a parameter? Or always run task?
